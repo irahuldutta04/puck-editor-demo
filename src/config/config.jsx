4490c0777dacdbd21662e923841a88120b22d75d
irahuldutta04 <rahul.dutta@techmantra.co>
2024-05-02 17:43:19 +0530

    push

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,5 +1,6 @@
 import { AboutBengal } from "../PageComponents/AboutBengal";
 import { NewAboutBengal } from "../PageComponents/NewAboutBengal";
+import { Tourism } from "../PageComponents/Tourism";
 import Section from "./components/Section";
 
 export const config = {
@@ -72,8 +73,8 @@ export const config = {
                   color === "default"
                     ? "inherit"
                     : color === "muted"
-                      ? "#666"
-                      : "red",
+                    ? "#666"
+                    : "red",
                 display: "flex",
                 textAlign: align,
                 width: "100%",
@@ -86,8 +87,8 @@ export const config = {
                   align === "center"
                     ? "center"
                     : align === "right"
-                      ? "flex-end"
-                      : "flex-start",
+                    ? "flex-end"
+                    : "flex-start",
               }}
             >
               {text}
@@ -208,8 +209,8 @@ export const config = {
     },
     Toursim: {
       fields: {
-        padding: { type: 'text' },
-        maxWidth: { type: 'text' },
+        padding: { type: "text" },
+        maxWidth: { type: "text" },
         topText: { type: "text" },
         HeaderPosition: {
           type: "radio",
@@ -225,27 +226,35 @@ export const config = {
           getItemSummary: (item, i) => item.title || `Feature #${i}`,
           defaultItemProps: {
             title: "Title",
-            description: "Description",
+            link: "link",
           },
           arrayFields: {
             title: { type: "text" },
-            description: { type: "text" },
+            link: { type: "text" },
           },
         },
       },
       defaultProps: {
         topText: "Tourism",
-        padding: '0px',
-        maxWidth: '1280px',
-        HeaderPosition: 'center',
-        Background_Image: 'https://img.asmedia.epimg.net/resizer/v2/MGLDLRBRJVHNFJXBK5V3ATY2OM.jpg?auth=3369b1f9a28936f517b34c9806707a3e81f904e9079dd5140a075afd2f654ad3&width=644&height=362&smart=true',
+        padding: "0px",
+        maxWidth: "1280px",
+        HeaderPosition: "center",
+        Background_Image:
+          "https://img.asmedia.epimg.net/resizer/v2/MGLDLRBRJVHNFJXBK5V3ATY2OM.jpg?auth=3369b1f9a28936f517b34c9806707a3e81f904e9079dd5140a075afd2f654ad3&width=644&height=362&smart=true",
         items: [
           {
-            title: "Stat",
-            description: "1,000",
+            title: "Mountain",
+            link: "https://images.unsplash.com/photo-1519681393784-d120267933ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          },
+          {
+            title: "Forest",
+            link: "https://images.unsplash.com/photo-1476231682828-37e571bc172f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          },
+          {
+            title: "Sea",
+            link: "https://images.unsplash.com/photo-1541617219835-3689726fa8e7?q=80&w=2126&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           },
         ],
-
       },
       render: ({
         topText,
@@ -253,25 +262,21 @@ export const config = {
         maxWidth,
         HeaderPosition,
         Background_Image,
-        items
+        items,
       }) => {
         return (
-          <Section padding={padding} maxWidth={maxWidth} style={{ backgroundImage: `url(${Background_Image})`, backgroudRepeat: 'no-repeat' }}>
-            <h2 style={{ textAlign: `${HeaderPosition}` }}>{topText}</h2>
-            <div>
-              <div>
-                {items.map((item, i) => (
-                  <div key={i}>
-                    <div style={{fontSize:'20px'}}>{item.title}</div>
-                    <div style={{ fontSize: '20px' }}>{item.description}</div>
-                  </div>
-                ))}
-              </div>
-            </div>
+          <Section padding={padding} maxWidth={maxWidth}>
+            <Tourism
+              {...{
+                topText,
+                HeaderPosition,
+                Background_Image,
+                items,
+              }}
+            />
           </Section>
-        )
-      }
-
-    }
+        );
+      },
+    },
   },
 };
