2daeaf762eca39def72024fdd539faef1acc3d48
Srijita <srijita.kumar@techmantra.co>
2024-05-02 17:49:01 +0530

    Relief Fund

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -256,5 +256,40 @@ export const config = {
         );
       },
     },
-  },
-};
+    // Relief Fund block
+    ReliefFund:{
+      fields: {
+        padding: { type: "text" },
+        maxWidth: { type: "text" },
+        topHeading: {type: 'text'},
+        middleText: {type: 'text'},
+        bottomText: {type: 'text'},
+        },
+        defaultProps:{
+          // padding: '24px',
+          maxWidth: '100%',
+          topHeading: 'RELIEF FUND',
+          middleText: 'The West Bengal Government appeals ALL to contribute in West Bengal State Emergency Relief Fund and assist the State Goveronment in prevention and control of situation arising out of unforeseen emergencies like COVID-19 (CORONA)',
+          bottomText: 'Contribution to this fund entities you to claim 100% deduction under section 80G of the income Tax Act.',
+        },
+        render:({maxWidth,topHeading,middleText,bottomText})=>{
+           return (
+            <Section maxWidth={maxWidth}>
+              <div className="reliefFund">
+                <h1 className="relheading">{topHeading}</h1>
+                <div className="paradiv">
+                  <p className="relpara">{middleText}</p>
+                  <p className="relpara">{bottomText}</p>
+                </div>
+                <div className="flex justify-center gap-8 mt-4 mb-4">
+                  <button className="relbtn1">KNOW MORE</button>
+                  <button className="relbtn2">DONATE</button>
+                </div>
+              </div>
+            </Section>
+           );
+        },
+      },
+    },
+  };
+
